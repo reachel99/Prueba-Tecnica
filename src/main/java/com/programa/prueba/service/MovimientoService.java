8b89520ff1758397c02d7fd283821e38ad7becca
reachel99 <156333850+reachel99@users.noreply.github.com>
2024-08-30 09:59:10 -0500

    MovimientoService.java

--- src/main/java/com/programa/prueba/service/MovimientoService.java
+++ src/main/java/com/programa/prueba/service/MovimientoService.java
@@ -1,62 +1,85 @@
-package com.programa.prueba.controller;
+package com.programa.prueba.service;
 
 import com.programa.prueba.exception.SaldoInsuficienteException;
+import com.programa.prueba.model.Cuenta;
 import com.programa.prueba.model.Movimiento;
-import com.programa.prueba.service.MovimientoService;
+import com.programa.prueba.repository.CuentaRepository;
+import com.programa.prueba.repository.MovimientoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovimientoService {
 
 
-
-@RestController
-@RequestMapping("/movimientos")
-@Validated
-public class MovimientoController {
     @Autowired
-    private MovimientoService movimientoService;
+    private MovimientoRepository movimientoRepository;
 
-    @PostMapping
-    public ResponseEntity<String> crearMovimiento(@RequestBody Movimiento movimiento) {
-        try {
-            movimientoService.crearMovimiento(movimiento);
-            return ResponseEntity.ok("Movimiento registrado con éxito");
-        } catch (SaldoInsuficienteException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    @Autowired
+    private CuentaRepository cuentaRepository;
+
+    public Movimiento crearMovimiento(Movimiento movimiento) {
+        Optional<Cuenta> cuentaOpt = cuentaRepository.findById(movimiento.getCuentaId());
+        if (cuentaOpt.isPresent()) {
+            Cuenta cuenta = cuentaOpt.get();
+
+            if ("Deposito".equalsIgnoreCase(movimiento.getTipo())) {
+
+                cuenta.setSaldoInicial(cuenta.getSaldoInicial().add(movimiento.getValor()));
+                cuentaRepository.save(cuenta);
+                movimiento.setSaldo(cuenta.getSaldoInicial());
+                return movimientoRepository.save(movimiento);
+            } else if ("Retiro".equalsIgnoreCase(movimiento.getTipo())) {
+                if (cuenta.getSaldoInicial().compareTo(movimiento.getValor()) < 0) {
+                    throw new SaldoInsuficienteException("Saldo no disponible");
+                }
+
+                cuenta.setSaldoInicial(cuenta.getSaldoInicial().subtract(movimiento.getValor()));
+                cuentaRepository.save(cuenta);
+                movimiento.setSaldo(cuenta.getSaldoInicial());
+                return movimientoRepository.save(movimiento);
+            } else {
+                throw new IllegalArgumentException("Tipo de movimiento no válido");
+            }
+        }
+        throw new IllegalArgumentException("Cuenta no encontrada");
+    }
+
+    public List<Movimiento> obtenerTodosMovimientos() {
+        return movimientoRepository.findAll();
+    }
+
+    public Movimiento obtenerMovimientoPorId(Long id) {
+        return movimientoRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public Movimiento actualizarMovimiento(Long id, Movimiento movimiento) {
+        Optional<Movimiento> movimientoExistente = movimientoRepository.findById(id);
+        if (movimientoExistente.isPresent()) {
+            Movimiento movToUpdate = movimientoExistente.get();
+
+  
+            if (!"Deposito".equalsIgnoreCase(movimiento.getTipo()) && !"Retiro".equalsIgnoreCase(movimiento.getTipo())) {
+                throw new IllegalArgumentException("Tipo de movimiento no válido");
+            }
+
+            movToUpdate.setFecha(movimiento.getFecha());
+            movToUpdate.setTipo(movimiento.getTipo());
+            movToUpdate.setValor(movimiento.getValor());
+            movToUpdate.setCuentaId(movimiento.getCuentaId());
+
+            return movimientoRepository.save(movToUpdate);
+        } else {
+            throw new RuntimeException("Movimiento no encontrado");
         }
     }
 
-    @GetMapping
-    public ResponseEntity<List<Movimiento>> obtenerTodosMovimientos() {
-        return ResponseEntity.ok(movimientoService.obtenerTodosMovimientos());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Movimiento> obtenerMovimientoPorId(@PathVariable Long id) {
-        Movimiento movimiento = movimientoService.obtenerMovimientoPorId(id);
-        if (movimiento != null) {
-            return ResponseEntity.ok(movimiento);
-        }
-        return ResponseEntity.notFound().build();
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Movimiento> actualizarMovimiento(@PathVariable Long id, @RequestBody Movimiento movimiento) {
-        Movimiento movimientoActualizado = movimientoService.actualizarMovimiento(id, movimiento);
-        if (movimientoActualizado != null) {
-            return ResponseEntity.ok(movimientoActualizado);
-        }
-        return ResponseEntity.notFound().build();
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminarMovimiento(@PathVariable Long id) {
-        movimientoService.eliminarMovimiento(id);
-        return ResponseEntity.noContent().build();
+    public void eliminarMovimiento(Long id) {
+        movimientoRepository.deleteById(id);
     }
 }
